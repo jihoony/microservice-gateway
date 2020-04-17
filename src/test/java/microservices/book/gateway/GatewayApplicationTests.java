5e9dceb0eab1fe1e4831c1ee64460b4335cb5773
jihoon <jihoon_yang@ksmartech.com>
2020-04-17 14:57:23 +0900

    first commit

---
+++ src/test/java/microservices/book/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package microservices.book.gateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
